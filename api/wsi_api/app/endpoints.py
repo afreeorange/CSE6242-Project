5167aac4cdc3f14dffd2c15b53870022b7a1f837
Yatish <yatish.kasaraneni@gatech.edu>
2019-10-31 22:20:33 -0400

    changes to API

--- api/wsi_api/app/endpoints.py
+++ api/wsi_api/app/endpoints.py
@@ -1,5 +1,6 @@
 from flask import render_template, jsonify
 from . import wsi_api_blueprint
+from .models import db, Parameter
 
 
 @wsi_api_blueprint.route('/')
@@ -13,3 +14,11 @@ def sample_endpoint():
         "message": "Hello from the API!",
         "errors": [],
     })
+
+@wsi_api_blueprint.route('/wsi/<int:year>')
+def wsi_endpoint(year):
+    print(year)
+    data = Parameter.query.filter(Parameter.VariableName == 'SDG 6.4.2. Water Stress', Parameter.Year == year).all()
+    
+    print(data)
+    return jsonify(json_list=[i.serialize for i in data])
--- api/wsi_api/factory.py
+++ api/wsi_api/factory.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app(debug=False):
     """wsi_api application factory
@@ -9,8 +11,14 @@ def create_app(debug=False):
     app = Flask(__name__)
     app.debug = debug
     CORS(app)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
     from .app import wsi_api_blueprint
     app.register_blueprint(wsi_api_blueprint)
+
+    with app.app_context():
+        db.create_all()
 
     return app
