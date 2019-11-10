5ad639117a4d7c09a715697e1268f9d46174f6c2
Yatish <yatish.kasaraneni@gatech.edu>
2019-11-10 11:22:41 -0500

    api chages with working endpoint

--- api/wsi_api/app/endpoints.py
+++ api/wsi_api/app/endpoints.py
@@ -1,6 +1,7 @@
 from flask import render_template, jsonify
 from . import wsi_api_blueprint
-from .models import db, Parameter
+import sqlite3
+from sqlite3 import Error
 
 
 @wsi_api_blueprint.route('/')
@@ -15,10 +16,53 @@ def sample_endpoint():
         "errors": [],
     })
 
+def create_connection():
+    sqlite3.enable_callback_tracebacks(True)
+    conn = None
+    try:
+        conn = sqlite3.connect("./project.db")
+    except Error as e:
+        print(e)
+ 
+    return conn
+
+def serialize_wsi_data(row):
+    return {
+        'name': row[0],
+        'wsivalue' : row[1]
+    }
+
 @wsi_api_blueprint.route('/wsi/<int:year>')
 def wsi_endpoint(year):
     print(year)
-    data = Parameter.query.filter(Parameter.VariableName == 'SDG 6.4.2. Water Stress', Parameter.Year == year).all()
+    conn = create_connection()
+    cur = conn.cursor()
+
+    rows = None
+    if(year==1980):
+        cur.execute("select country.Name, wsi.Y1980 from wsi join country on country.AreaId = wsi.AreaId")
+    elif(year==1985):
+        cur.execute("select country.Name, wsi.Y1985 from wsi join country on country.AreaId = wsi.AreaId")
+    elif(year==1990):
+        cur.execute("select country.Name, wsi.Y1990 from wsi join country on country.AreaId = wsi.AreaId")
+    elif(year==1995):
+        cur.execute("select country.Name, wsi.Y1995 from wsi join country on country.AreaId = wsi.AreaId")
+    elif(year==2000):
+        cur.execute("select country.Name, wsi.Y2000 from wsi join country on country.AreaId = wsi.AreaId")
+    elif(year==2005):
+        cur.execute("select country.Name, wsi.Y2005 from wsi join country on country.AreaId = wsi.AreaId")
+    elif(year==2010):
+        cur.execute("select country.Name, wsi.Y2010 from wsi join country on country.AreaId = wsi.AreaId")
+    elif(year==2015):
+        cur.execute("select country.Name, wsi.Y2015 from wsi join country on country.AreaId = wsi.AreaId")
     
-    print(data)
-    return jsonify(json_list=[i.serialize for i in data])
+    rows = cur.fetchall()
+
+    return (jsonify(json_list = [serialize_wsi_data(i) for i in rows]))
+
+@wsi_api_blueprint.route('/calculate/<int:indicator>')
+def calculate_wsi_endpoint(indicator):
+    return jsonify({
+        "message": "Work in progress",
+        "errors": [],
+    })
--- api/wsi_api/factory.py
+++ api/wsi_api/factory.py
@@ -1,24 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
 
 def create_app(debug=False):
     """wsi_api application factory
     """
-
     app = Flask(__name__)
     app.debug = debug
     CORS(app)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
-
+    
     from .app import wsi_api_blueprint
     app.register_blueprint(wsi_api_blueprint)
-
-    with app.app_context():
-        db.create_all()
 
     return app
