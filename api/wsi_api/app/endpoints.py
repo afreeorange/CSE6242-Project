381ab716d1568b392f209aac51ce2be84abb23eb
Yatish <yatish.kasaraneni@perficient.com>
2019-11-21 19:24:20 -0500

    Updated endpoints for wsi

--- api/wsi_api/app/endpoints.py
+++ api/wsi_api/app/endpoints.py
@@ -26,39 +26,49 @@ def create_connection():
  
     return conn
 
-def serialize_wsi_data(row):
-    return {
-        'name': row[0],
-        'wsivalue' : row[1]
-    }
+def serialize_wsi_data(rows):
+    returnDict = {}
+    wsi1980Dict = {}
+    wsi1985Dict = {}
+    wsi1990Dict = {}
+    wsi1995Dict = {}
+    wsi2000Dict = {}
+    wsi2005Dict = {}
+    wsi2010Dict = {}
+    wsi2015Dict = {}
+    for x in rows:
+        wsi1980Dict[x[0]] = x[1]
+        wsi1985Dict[x[0]] = x[2]
+        wsi1990Dict[x[0]] = x[3]
+        wsi1995Dict[x[0]] = x[4]
+        wsi2000Dict[x[0]] = x[5]
+        wsi2005Dict[x[0]] = x[6]
+        wsi2010Dict[x[0]] = x[7]
+        wsi2015Dict[x[0]] = x[8]
 
-@wsi_api_blueprint.route('/wsi/<int:year>')
-def wsi_endpoint(year):
-    print(year)
+    returnDict['1980'] = wsi1980Dict
+    returnDict['1985'] = wsi1985Dict
+    returnDict['1990'] = wsi1990Dict
+    returnDict['1995'] = wsi1995Dict
+    returnDict['2000'] = wsi2000Dict
+    returnDict['2005'] = wsi2005Dict
+    returnDict['2010'] = wsi2010Dict
+    returnDict['2015'] = wsi2015Dict
+
+    return returnDict
+
+@wsi_api_blueprint.route('/wsi')
+def wsi_endpoint():
+    #print(year)
     conn = create_connection()
     cur = conn.cursor()
 
     rows = None
-    if(year==1980):
-        cur.execute("select country.Name, wsi.Y1980 from wsi join country on country.AreaId = wsi.AreaId")
-    elif(year==1985):
-        cur.execute("select country.Name, wsi.Y1985 from wsi join country on country.AreaId = wsi.AreaId")
-    elif(year==1990):
-        cur.execute("select country.Name, wsi.Y1990 from wsi join country on country.AreaId = wsi.AreaId")
-    elif(year==1995):
-        cur.execute("select country.Name, wsi.Y1995 from wsi join country on country.AreaId = wsi.AreaId")
-    elif(year==2000):
-        cur.execute("select country.Name, wsi.Y2000 from wsi join country on country.AreaId = wsi.AreaId")
-    elif(year==2005):
-        cur.execute("select country.Name, wsi.Y2005 from wsi join country on country.AreaId = wsi.AreaId")
-    elif(year==2010):
-        cur.execute("select country.Name, wsi.Y2010 from wsi join country on country.AreaId = wsi.AreaId")
-    elif(year==2015):
-        cur.execute("select country.Name, wsi.Y2015 from wsi join country on country.AreaId = wsi.AreaId")
+    cur.execute("select country.Name, wsi.Y1980, wsi.Y1985, wsi.Y1990, wsi.Y1995, wsi.Y2000, wsi.Y2005, wsi.Y2010, wsi.Y2015 from wsi join country on country.AreaId = wsi.AreaId")
     
     rows = cur.fetchall()
 
-    return (jsonify(json_list = [serialize_wsi_data(i) for i in rows]))
+    return (jsonify(serialize_wsi_data(rows)))
 
 @wsi_api_blueprint.route('/calculate/<int:indicator>')
 def calculate_wsi_endpoint(indicator):
