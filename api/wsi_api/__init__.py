4492e933725d69499d5bdcc663f5ffc1be26ff83
Nikhil Anand <mail@nikhil.io>
2019-11-28 14:40:25 -0600

    More cleanup

--- api/wsi_api/__init__.py
+++ api/wsi_api/__init__.py
@@ -1,23 +1,10 @@
 import os
 import sqlite3
 import sys
-from datetime import datetime
 from sqlite3 import Error as ConnectionError
 
 from .factory import create_app
 
-__title__ = 'wsi_api'
-__version__ = '0.0.1',
-__author__ = 'Team 102'
-__author_email__ = 'nikhil.anand@gatech.edu'
-__license__ = 'MIT'
-__copyright__ = '(c) {}'.format(datetime.now().year)
-__url__ = 'https://github.gatech.edu/sprice31/cse6242fall2019team102'
-__description__ = """
-Upstream API for an app that shows Global  Water Stress Index
-"""
-
-# Initialize a DB connection
 sqlite3.enable_callback_tracebacks(True)
 db_connection = None
 
